64b9a573793fda4464ee542993a6734bed837a7e
Luis Osa <luis.osa.gdc@gmail.com>
2013-09-22 18:08:13 +0200

    Add tests on queue pop and push without member

--- test/ut/queue-test.js
+++ test/ut/queue-test.js
@@ -66,4 +66,106 @@ describe("Queue resource", function() {
                     .end(done)
             };
         })
+
+    it("returns an error if no member is present when pushing",
+        function(done) {
+            agent.post("/api/queue/foo")
+                .send({
+                    "action" : "push",
+                })
+                .expect(400)
+                .expect({
+                  "errorCode" : "QUE-01",
+                  "errorMessage" : "Las operaciones de insercción deben tener " +
+                                   "un número de teléfono" 
+                })
+                .end(done)
+        })
+
+    it("must return an updated view of the queue on GET",
+        function(done) {
+            agent.post("/api/queue/grr")
+                .send({
+                    "action" : "push",
+                    "member" : {
+                        "phoneNumber" : "string"
+                    }
+                })
+                .expect(200)
+                .end(postAgain)
+            function postAgain(err, req) {
+                agent.post("/api/queue/grr")
+                    .send({
+                        "action" : "push",
+                        "member" : {
+                            "phoneNumber" : "another"
+                        }
+                    })
+                    .expect(200)
+                    .end(retrieveAgain)
+            };
+            function retrieveAgain(err, req) {
+                agent.get("/api/queue/grr")
+                    .expect(200)
+                    .expect({
+                        "id": "grr",
+                        "name": "La cola de grr",
+                        "members": [
+                            {"phoneNumber": "string"},
+                            {"phoneNumber": "another"}
+                        ]
+                    })
+                    .end(done)
+            };
+        })
+
+    it("must not allow for multiple pushes in one request",
+        function(done) {
+            agent.post("/api/queue/brr")
+                .send({
+                    "action": "push",
+                    "member": {"phoneNumber": "string"},
+                    "member": {"phoneNumber": "strong"}})
+                .expect(400)
+                .end(done)
+        })
+
+    it("must allow to pop the first added member",
+        function(done) {
+            agent.post("/api/queue/gro")
+                .send({
+                    "action" : "push",
+                    "member" : {
+                        "phoneNumber" : "first"
+                    }
+                })
+                .expect(200)
+                .end(postAgain)
+            function postAgain(err, req) {
+                agent.post("/api/queue/gro")
+                    .send({
+                        "action" : "push",
+                        "member" : {
+                            "phoneNumber" : "second"
+                        }
+                    })
+                    .expect(200)
+                    .end(popTheFirst)
+            };
+            function popTheFirst(err, req) {
+                agent.post("/api/queue/gro")
+                    .send({
+                        "action": "pop"
+                    })
+                    .expect(200)
+                    .expect({
+                        "id": "gro",
+                        "name": "La cola de gro",
+                        "members": [
+                            {"phoneNumber": "second"}
+                        ]
+                    })
+                    .end(done)
+            }
+        })
 });
